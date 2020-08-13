affb74ad81e627a86726c04d3092fdc3a3873dc0
Gagandeep07_Kaur <gagandeep07kaur@gmail.com>
2020-08-13 22:11:38 +0530

    used forEach loop

---
+++ Ecma_properties/forEach.js
@@ -0,0 +1,10 @@
+/*
+It executes the function for every element found in the array.
+*/
+
+let arr = [1,2,3,4,5];
+
+arr.forEach(function(element) {
+    console.log(element); //prints all the elements of the array
+})
+
