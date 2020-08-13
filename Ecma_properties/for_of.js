6ed9a0b688f362f50ba0426e8935a2ee10a11559
Gagandeep07_Kaur <gagandeep07kaur@gmail.com>
2020-08-13 22:09:57 +0530

    used for..of loop iteration

---
+++ Ecma_properties/for_of.js
@@ -0,0 +1,11 @@
+/*
+For..Of :
+is an alternative for both for...in and forEach() 
+and loops iterable data structures like Arrays, Maps, Sets, and strings.
+*/
+
+const arr = ['one', 'two', 'three'];
+
+for(const a of arr) {
+    console.log(a);
+}
