28fb4c7f64374df219804435106fa40239c53983
Gagandeep07_Kaur <gagandeep07kaur@gmail.com>
2020-08-14 09:37:51 +0530

    used isArray method

---
+++ Ecma_properties/isArray.js
@@ -0,0 +1,8 @@
+/*
+This method checks if the object is an array or not and returns the result in true or false.
+*/
+
+let arr = [1,2,3,4,5,];
+let a = 5;
+console.log(Array.isArray(arr)); //returns true
+console.log(Array.isArray(a)); //returns false
