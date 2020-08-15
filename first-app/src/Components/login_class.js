07276372b03c149805f2b3f87eb44e399e575b9c
Gagandeep07_Kaur <gagandeep07kaur@gmail.com>
2020-08-15 16:53:59 +0530

    created username and password as state in Login component (class based)

--- first-app/src/Components/login_class.js
+++ first-app/src/Components/login_class.js
@@ -1,6 +1,14 @@
 import React, {Component} from 'react';
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+          username:'',
+          password:''
+        };
+      }
+    
   render() {
     return (
         <h3> Login Page</h3>
