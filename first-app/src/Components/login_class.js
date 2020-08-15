602f95d752c60ac19400bea77e2655df8a60365e
Gagandeep07_Kaur <gagandeep07kaur@gmail.com>
2020-08-15 16:49:11 +0530

    created login class component

---
+++ first-app/src/Components/login_class.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+        <h3> Login Page</h3>
+    ); 
+    
+  }
+}
+export default Login;
