c8bb91425ead5bf6e7a9b5f32df4c1ef5e0cf456
Gagandeep07_Kaur <gagandeep07kaur@gmail.com>
2020-08-19 21:55:19 +0530

    On changing the value in text area, displayed 'Too long', 'Too short', and 'Ok' status

--- first-app/src/Components/validation.js
+++ first-app/src/Components/validation.js
@@ -7,17 +7,27 @@ export default class Validation extends React.Component {
             inputValue: '' };
     }
 
-  
+    handleChange = (event) => {
+        this.setState({inputValue: event.target.value });
+    }
 
     render() {
-           
+        let header = '';
+        if (this.state.inputValue.length > 10) {
+            header = <h1>Too long </h1>
+        } else if (this.state.inputValue.length < 5) {
+            header = <h1> Too short </h1>
+        }    else {
+            header = <h1> OKK </h1>
+        }       
+    
 
         return (
         <div>
-                  
+            {header}        
         <input 
         type= 'text' 
-        placeholder= "Type Something..."/>
+        onChange={this.handleChange} placeholder= "Type Something..."/>
         
         </div>
         );
