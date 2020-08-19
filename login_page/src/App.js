f4483b201de2af1ba1d4202c01b3dc4546b32756
Gagandeep07_Kaur <gagandeep07kaur@gmail.com>
2020-08-19 22:21:31 +0530

    created login component

--- login_page/src/App.js
+++ login_page/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './Components/login_class.js';
 
 
 class App extends Component {
@@ -13,6 +14,8 @@ class App extends Component {
       <div className="App">
       
             <h1>{greeting}</h1>  
+
+            <Login value="Login an account" />
        
       
       </div>
---
+++ login_page/src/Components/login_class.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+
+
+class Login extends React.Component {
+  constructor(props) { //accepts props as input
+    super(props); //to pass down whatever is being inherited from the parent class
+    this.state= {
+      username:'',
+      password:'',
+               
+    };
+  }  
+     
+
+     render()
+     {     
+      
+      let {showSuccess} = this.state;
+     
+      return (                           
+           
+        <div className="App">  
+
+               
+           
+          <h2>{this.props.value}.....</h2>    <br/>                           
+                               
+             <label for="username">
+               Username:
+               <input type="text" id="username" name="username"/>
+             </label> <br/>
+             <label for="pass">
+               Password:
+               <input type="password" id="pass" name="password"/>
+             </label> <br>
+             </br> <br></br>    
+
+             <button>
+             Login 
+             </button>                                 
+
+        
+        </div> 
+          
+      );
+    }
+}
+
+export default Login;
