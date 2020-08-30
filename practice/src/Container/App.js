d21952ab57ce0e62b5c29d11b6cdd8c4e580f45d
Gagandeep07_Kaur <gagandeep07kaur@gmail.com>
2020-08-30 21:21:41 +0530

    modified App.js as class component

--- practice/src/Container/App.js
+++ practice/src/Container/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 
-function App() {
+class App extends Component {
+
+  render() {
+
+ 
   return (
     <div className="App">
        <h1>Hello</h1>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
