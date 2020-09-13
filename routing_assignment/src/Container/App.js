15b86ea56ebcb2e915203b2e287e574276dc3c3c
Gagandeep07_Kaur <gagandeep07kaur@gmail.com>
2020-09-13 13:22:23 +0530

    added another component and NavLink

--- routing_assignment/src/Container/App.js
+++ routing_assignment/src/Container/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import Blog from './Blog';
 
 class App extends Component {
   render () {
---
+++ routing_assignment/src/Container/Blog.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+import { Route, NavLink, Switch } from 'react-router-dom';
+
+class Blog extends Component {
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      data: [],
+      persons: [
+        { id: 'asfa1', name: 'Max', age: 28 },
+        { id: 'vasdf1', name: 'Manu', age: 29 },
+        { id: 'asdf11', name: 'Stephanie', age: 26 }
+      ],
+      showPersons: false,
+      value: '',
+      data1: 'Initial data...',
+    }
+    
+  };
+
+  
+    render() {   
+      
+    return(
+      <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink
+                                to="/persons/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Persons</NavLink></li>
+
+                                <li><NavLink to={{
+                                pathname: '/dyn_table'                                
+                            }}>Dynamic Table</NavLink></li>
+
+                            <li><NavLink
+                                to="/api_table"
+                                exact
+                                activeClassName="hurray"
+                                activeStyle={{
+                                    color: '#fa923f',                                    
+                                }}>Api_Table</NavLink></li>
+
+                                 <li><NavLink to={{
+                                pathname: '/api_list'                                
+                            }}>Api_List</NavLink></li>
+                           
+                        </ul>
+                    </nav>
+                </header>     
+
+
+                <Switch>
+                                     
+                </Switch>
+            </div>
+       
+    )
+  }
+}
+
+function select(state) {
+  return {
+     visibleTodos: state.todos
+  }
+}
+
+export default Blog;
+  
