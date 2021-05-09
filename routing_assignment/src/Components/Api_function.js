14da1f6426009a0bd0983973d16ba0c9a29a3cd8
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-05-09 16:39:48 +0530

    fetching api using function component

--- routing_assignment/src/Components/Api_function.js
+++ routing_assignment/src/Components/Api_function.js
@@ -2,73 +2,55 @@ import React, { useState, useEffect } from 'react';
 import './Api.css';
 
 export default function Api_function() {
-    const [state, setState] = useState([])
     const [items, setItems] = useState([]);
-    const [hasError, setHasError] = useState(false)
-    const [loading, setLoading] = useState(false)
 
+    const apiUrl = "https://jsonplaceholder.typicode.com/posts";
+
+    //componentDidMount
     useEffect(() => {
-        setLoading(true)
-        fetch("https://jsonplaceholder.typicode.com/users").then(
-            res => {
-                setState(res.data);
-                setLoading(false)}               
-        )        
+        fetch(apiUrl)
+          .then(res => res.json())
+          .then(res => {
+              setItems(res);
+          })                
         .catch(err => {
-            setHasError(true)
-            setLoading(false)
+            console.log(err);
         })
-    }, [])
-   
-    
-    return (
-       
-        // <div>
-        //     <h4 id='dataTitle'><b><u>React API calling </u></b> : In Table format </h4>
-        //        <table id='data'>
-        //            <tbody>
-        //               <tr>
-        //                 <th>ID</th>
-        //                 <th>Title</th>
-        //                 <th>Body</th>                           
-        //               </tr>
-        //                {/* {this.renderTableData()} */}
-        //            </tbody>
-        //        </table>      
-        // </div>
-        <div>
-        <h3><b><u>Calling API - </u></b> using functional component</h3>
-            {/* <table id='data'>
-                <tbody>
-                {items.map(item => (
-                  <tr key={item.id}>
-                    <th>ID</th>
-                    <td>{item.id}</td>
-                    <th>Title</th>
-                    <th>Body</th>                           
-                  </tr>
-                  
-                  
-                ))}
-                </tbody>
-            </table>   */}
-        {/* <ul>
-        {items.map(item => (
-            <li key={item.id}>
-                <b>Id:</b> {item.id};
-                <b> Name: </b> {item.name} ; 
-                <b> Email: </b> {item.email};
-                <b> Phone: </b> {item.phone}
-            </li>
-        ))}
-        </ul> */}
+    });
 
-        {
-                loading ? <div>Loading...</div> : hasError ? <div>Error occured.</div> : (items.map( item => <div>{item.name}</div>))
-            }
-    </div>
-        
-        
-      ); 
+    const Tr = ({item}) => { 
+        return (
+            <tr key = {item.id}>
+                <td> {item.id} </td>
+                <td> {item.title} </td>
+                <td> {item.body} </td>
+            </tr>
+        );
+    }
+
+   
+    return (
+        <div>
+            <h3 id='dataTitle'><b><u>Calling API - </u></b> using functional component</h3>
+            Items: {items.length}
+            <table id='data'>
+                <thead>
+                  <tr>
+                    <th>Id</th>
+                    <th>Title</th>
+                    <th>Body</th>
+                  </tr>
+                </thead>
+
+              <tbody>
+                {items.map((i,id) => {
+                  return <Tr key={id} item={i} />
+                })}
+              </tbody>
+            </table>
+     
+        </div>
+    ); 
     
 }
+
